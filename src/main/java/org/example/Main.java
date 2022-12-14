37fa65494cb42af4450f76036fb3d4d0502a6f46
itllsendamsg <marcin.chudy.office@gmail.com>
2022-12-14 22:52:04 +0100

    14 12 22 ENHKX Lifeline Drone: Commit #1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println();
         System.out.println("Hello world!");
+        System.out.println();
+        System.out.println("ENHKX Lifeline Drone Essa :-D Hello World Marcin!");
     }
 }
